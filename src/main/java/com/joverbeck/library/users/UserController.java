52a97c135dede58422e44bb0d41a90b9af78d4b3
overbejt <overbejt@miamioh.edu>
2020-07-14 17:15:25 -0400

    Added a response entity for the signup controller.  That way the front end will know it is successful or not

--- src/main/java/com/joverbeck/library/users/UserController.java
+++ src/main/java/com/joverbeck/library/users/UserController.java
@@ -1,10 +1,12 @@
 package com.joverbeck.library.users;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/users")
@@ -26,10 +28,11 @@ public class UserController {
 	 * @param user The user object from the post body.
 	 */
 	@PostMapping("/sign-up")
-	public void signup (@RequestBody MyUser user) {
+	public ResponseEntity<?> signup (@RequestBody MyUser user) {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		myUserRepository.save(user);
 		System.out.println("=== New user was created ===");
+		return ResponseEntity.ok("{}");
 	}  // End of the 'signup' method
 	
 }  // End of the 'UserController' class
