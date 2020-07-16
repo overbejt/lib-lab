8eb7b6394aa9aa0afec541b84f22abc858705f7a
overbejt <overbejt@miamioh.edu>
2020-07-16 03:37:10 -0400

    asdf

--- src/main/java/com/joverbeck/library/security/WebSecurity.java
+++ src/main/java/com/joverbeck/library/security/WebSecurity.java
@@ -29,7 +29,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/books/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/books/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
